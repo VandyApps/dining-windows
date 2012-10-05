44c4710991f023515e098a6b3857c348c028cb4d
Andrew Lum <andrew.lum01@hotmail.com>
2012-10-05 18:58:03 -0500

    Panorama, Database is set up correctly.
    Please note that this is a different project name than the first 2.
    Please open this one in visual studios and save a local copy on your
    machine for editing.
    Linq to Sql!

---
+++ Windows Dining/Windows Dining/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+
+namespace Windows_Dining
+{
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        // Load data for the ViewModel Items
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void RestaurantListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedName = App.ViewModel.Items.ElementAt(RestaurantListBox.SelectedIndex).LineOne;
+            NavigationService.Navigate(new Uri("/DiningDetail.xaml?name=" + selectedName, UriKind.Relative));
+        }
+    }
+}
---
+++ Windows Dining/Windows Dining/ViewModels/MainViewModel.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using Microsoft.Phone.Data.Linq;
+using Microsoft.Phone.Data.Linq.Mapping;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+
+namespace Windows_Dining
+{
+
+    public class DBHelper
+    {
+        private const string ConnectionString = "isostore:/dining.sdf";
+
+        public static void CreateDatabase()
+        {
+            using (var context = new Dining(ConnectionString))
+            {
+                if (!context.DatabaseExists())
+                {
+                    // create database if it does not exist
+                    context.CreateDatabase();
+                }
+            }
+        }
+
+        public static void DeleteDatabase()
+        {
+            using (var context = new Dining(ConnectionString))
+            {
+                if (context.DatabaseExists())
+                {
+                    // delete database if it exist
+                    context.DeleteDatabase();
+                }
+            }
+        }
+
+        public static IList<Dining1> GetRestaurants()
+        {
+            IList<Dining1> restaurants;
+            using (var context = new Dining(ConnectionString))
+            {
+                restaurants = (from Dining1 d in context.Restaurants select d).ToList();
+            }
+
+            return restaurants;
+        }
+
+        public static void MoveReferenceDatabase()
+        {
+            // Obtain the virtual store for the application.
+            IsolatedStorageFile iso = IsolatedStorageFile.GetUserStoreForApplication();
+
+            // Create a stream for the file in the installation folder.
+            using (Stream input = Application.GetResourceStream(new Uri("ViewModels/dining.sdf", UriKind.Relative)).Stream)
+            {
+                // Create a stream for the new file in isolated storage.
+                using (IsolatedStorageFileStream output = iso.CreateFile("dining.sdf"))
+                {
+                    // Initialize the buffer.
+                    byte[] readBuffer = new byte[4096];
+                    int bytesRead = -1;
+
+                    // Copy the file from the installation folder to isolated storage. 
+                    while ((bytesRead = input.Read(readBuffer, 0, readBuffer.Length)) > 0)
+                    {
+                        output.Write(readBuffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
+    }
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public void LoadData()
+        {
+
+            DBHelper.MoveReferenceDatabase();
+            IList<Dining1> restaurants = DBHelper.GetRestaurants();
+
+            for (int i = 0; i < restaurants.Count(); i++)
+            {
+                this.Items.Add(new ItemViewModel() { LineOne = restaurants[i].Name, LineTwo = restaurants[i].Type });
+            }
+          
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
